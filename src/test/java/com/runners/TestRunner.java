6c05792f8a29df126e00318684e4bf88c4becad1
cschmertz <cschmertz@gmail.com>
2024-10-12 13:39:53 -0400

    runner fix

--- src/test/java/com/runners/TestRunner.java
+++ src/test/java/com/runners/TestRunner.java
@@ -1,23 +1,20 @@
 package com.runners;
 
-import io.cucumber.junit.Cucumber;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.ConfigurationParameter;
 
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+//import static io.cucumber.junit.platform.engine.Constants.TAGS_PROPERTY_NAME;
 
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = {"src/test/resources/functionalTests"},
-        glue= {"stepDefinitions"},
-        plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
-                "junit:target/cucumber-reports/Cucumber.xml",
-                "html:target/cucumber-report.html",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "listeners.TestFailureListener"},
-        monochrome = true,
-        tags = "@Google",
-        dryRun = false
-)
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features") // Path to your feature files
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepDefinitions") // Step definitions package
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber-reports/Cucumber.json, junit:target/cucumber-reports/Cucumber.xml, html:target/cucumber-report.html, com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:, listeners.TestFailureListener") // Report generation and custom listeners
+//@ConfigurationParameter(key = TAGS_PROPERTY_NAME, value = "@Google") // Filters scenarios based on the tag
 public class TestRunner {
 }
+
